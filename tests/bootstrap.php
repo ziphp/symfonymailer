d8e9a3d28292d82ed553583fafb2742d90661d54
charescape <53265646+charescape@users.noreply.github.com>
2023-11-15 17:48:48 +0800

    rename

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
 require_once(__DIR__ . '/../vendor/autoload.php');
-require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require_once(__DIR__ . '/../vendor/ziphp/ziphp/framework/Yii.php');
 
 Yii::setAlias('@yiiunit/extensions/symfonymailer', __DIR__);
 Yii::setAlias('@yii/symfonymailer', dirname(__DIR__) . '/src');
